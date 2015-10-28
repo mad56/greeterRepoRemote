1e039070d83e1805ce7879cda90a1692dc9badd2
Madeleine <Madeleine@192.168.10.37>
2015-10-28 11:45:39 +0100

    ras
    Signed-off-by: Madeleine <Madeleine@192.168.10.37>

--- greeter/src/main/java/edu/esprit/greeter/Greeter.java
+++ greeter/src/main/java/edu/esprit/greeter/Greeter.java
@@ -2,6 +2,16 @@ package edu.esprit.greeter;
 
 public class Greeter {
 	
+	/**
+	 * 
+	 * sayHello method
+	 * 
+	 * @param name
+	 * @return "Hello "+name
+	 */ 
+	
+	
+	
 	public String sayHello (String name){
 		
 		if(name.equals("MAN")){
